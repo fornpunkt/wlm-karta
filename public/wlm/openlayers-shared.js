e397b762849b2a17554f57600f6c2c58352c26ce
Abbe98 <albin.post@gmail.com>
2025-09-02 19:14:14 +0200

    fix: update fp map layers

--- public/wlm/openlayers-shared.js
+++ public/wlm/openlayers-shared.js
@@ -104,7 +104,7 @@ builtInBaseLayers.push(new Layer(
 builtInBaseLayers.push(new Layer(
   'WMS',
   {
-    url: 'https://api.lantmateriet.se/historiska-ortofoton/wms/v1/token/19075f4e-1929-323d-a7d1-5c1753904db2/?',
+    url: '/lm/proxy/historiska-ortofoton/wms/v1?',
     crossOrigin: '',
     params: {'LAYERS': 'OI.Histortho_60'},
     serverType: 'geoserver',
@@ -116,23 +116,13 @@ builtInBaseLayers.push(new Layer(
 builtInBaseLayers.push(new Layer(
   'WMS',
   {
-    url: 'https://api.lantmateriet.se/historiska-ortofoton/wms/v1/token/19075f4e-1929-323d-a7d1-5c1753904db2/?',
+    url: '/lm/proxy/historiska-ortofoton/wms/v1?',
     crossOrigin: '',
     params: {'LAYERS': 'OI.Histortho_75'},
     serverType: 'geoserver',
     projection: 'EPSG:3857',
   },
   'Ortofoto 1970/80-tal',
-));
-
-builtInBaseLayers.push(new Layer(
-  'WMTS',
-  {
-    url: 'https://api.lantmateriet.se/open/topowebb-ccby/v1/wmts/token/9b342b7d9f12d4ddb92277be9869d860/?SERVICE=WMTS&request=GetCapabilities',
-    layer: 'topowebb',
-    projection: 'EPSG:3857',
-  },
-  'LM Topografiska',
 ));
 
 function findLayerByName(name) {
